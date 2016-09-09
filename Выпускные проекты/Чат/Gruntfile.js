8c042aa790eb304ef74f93be7e4f72b9d3fb7ecb
VorgSpring <drmrSP@yandex.ru>
2016-09-09 22:22:15 +0300

    добавил файлы less, package.json и Gruntfile.js

---
+++ Выпускные проекты/Чат/Gruntfile.js
@@ -0,0 +1,65 @@
+"use strict";
+
+module.exports = function (grunt) {
+    require("load-grunt-tasks")(grunt);
+
+    grunt.initConfig({
+        less: {
+            style: {
+                files: {
+                    "css/style.css": "less/style.less"
+                }
+            }
+        },
+
+        postcss: {
+            options: {
+                processors: [
+                  require("autoprefixer")({
+                      browsers: [
+                        "last 1 version",
+                        "last 2 Chrome versions",
+                        "last 2 Firefox versions",
+                        "last 2 Opera versions",
+                        "last 2 Edge versions"
+                      ]
+                  })
+                ]
+            },
+            style: {
+                src: "css/*.css"
+            }
+        },
+
+        webpack: {
+            a: {
+                entry: "./js/src/main.js",
+                output: {
+                    path: "js",
+                    filename: "script.js"
+                }
+            }
+        },
+
+        watch: {
+            less: {
+                files: ["less/**/*.less"],
+                tasks: ["less", "postcss"],
+                options: {
+                    spawn: false
+                }
+            },
+
+            scripts: {
+                files: ["js/src/**/*.js"],
+                tasks: ["webpack"],
+                options: {
+                    spawn: false
+                }
+            }
+
+        }
+    });
+
+    grunt.registerTask("serve", ["watch"]);
+};
